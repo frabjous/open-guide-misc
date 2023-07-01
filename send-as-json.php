69428fd8c307a932829f1ba408b08a345d5ae4b8
Kevin Klement <klement@umass.edu>
2023-06-30 23:38:27 -0400

    file description on each one

--- send-as-json.php
+++ send-as-json.php
@@ -3,6 +3,11 @@
 // Public License along with this program. If not, see 
 // https://www.gnu.org/licenses/.
 
+////////////////////// send-as-json.php /////////////////////////////////
+// Simple and straightforward functions for relay json back to browser //
+/////////////////////////////////////////////////////////////////////////
+
+// defines a few standard http reponses
 const HTTP_STATUSES = [
     200 => 'OK',
     400 => 'Bad Request',
@@ -10,6 +15,7 @@ const HTTP_STATUSES = [
     405 => 'Method Not Allowed'
 ];
 
+// create an error in the response and send it
 function rage_quit($obj, $error = 'Unknown error', $status = 400) {
     if (!$obj instanceof StdClass) {
         $obj = new StdClass();
@@ -20,6 +26,7 @@ function rage_quit($obj, $error = 'Unknown error', $status = 400) {
     exit;
 }
 
+// outputs an object as json
 function send_as_json($obj, $status = 200) {
     header('HTTP/1.1 ' . strval($status) . ' ' . HTTP_STATUSES[$status]);
     header('Content-Type: application/json');
